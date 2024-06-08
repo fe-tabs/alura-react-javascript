4984125ce8f551cb8b408dd611e8bcf13fe08f62
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-06-08 17:25:39 -0300

    :sparkles: feat: Banner component

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+export default function Banner() {
+return (
+  <header className='banner'>
+    <img 
+      src="/img/banner.png" 
+      alt="Banner da página do Organo"  
+    />
+  </header>
+);
+}
